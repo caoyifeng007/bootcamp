55aef8c8c851d213530c8dbe06122165e5759f0d
ElvinsJ <caoyifeng007@163.com>
2022-08-12 16:24:34 +0800

    添加accounts task可以执行npx hardhat accounts来查看所有account

--- week5/hardhat-learning/hardhat.config.ts
+++ week5/hardhat-learning/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import { task } from "hardhat/config";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 export default config;
