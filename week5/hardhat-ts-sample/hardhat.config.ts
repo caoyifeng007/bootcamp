7dbb812245c627c0916e78048d32c7558337af57
ElvinsJ <caoyifeng007@163.com>
2022-08-16 10:13:09 +0800

    BlockNumber case coverage

--- week5/hardhat-ts-sample/hardhat.config.ts
+++ week5/hardhat-ts-sample/hardhat.config.ts
@@ -7,28 +7,28 @@ import "@nomiclabs/hardhat-solhint";
 import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.9",
-    //   defaultNetwork: "rinkeby",
-    networks: {
-        hardhat: {
-            forking: {
-                // url: `${process.env.alchemyKey}`,
-                url: `${process.env.infuraKey}`,
-                blockNumber: 11200000,
-            },
-        },
-        // rinkeby: {
-        //     url:`${process.env.infuraKey}`,
-        //     accounts: ["de982a3f252280315f34f5233b2be61b89e7e6e6801c4b04a4bfa0c1c7d31fda","0afa5ec8e9f6b17506328e789216c3332a91ecd6b682dbf66fc5cf728e918b31"]
-        //   }
+  solidity: "0.8.9",
+  //   defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `${process.env.alchemyKey}`,
+        // url: `${process.env.infuraKey}`,
+        blockNumber: 11200000,
+      },
     },
+    // rinkeby: {
+    //     url:`${process.env.infuraKey}`,
+    //     accounts: ["de982a3f252280315f34f5233b2be61b89e7e6e6801c4b04a4bfa0c1c7d31fda","0afa5ec8e9f6b17506328e789216c3332a91ecd6b682dbf66fc5cf728e918b31"]
+    //   }
+  },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
-    for (const account of accounts) {
-        console.log(account.address);
-    }
+  for (const account of accounts) {
+    console.log(account.address);
+  }
 });
 export default config;
--- week5/hardhat-ts-sample/test/BlockNumber.ts
+++ week5/hardhat-ts-sample/test/BlockNumber.ts
@@ -6,63 +6,106 @@ import { JsonRpcProvider, BaseProvider } from "@ethersproject/providers";
 
 const { utils, BigNumber } = ethers;
 
-describe("BlockNumber", function () {
-    let contract: BlockNumber;
-    let accounts: SignerWithAddress[];
-    let provider: JsonRpcProvider;
+describe("BlockNumber", function() {
+  let contract: BlockNumber;
+  let accounts: SignerWithAddress[];
+  let provider: JsonRpcProvider;
 
-    beforeEach(async function () {
-        const factory = await ethers.getContractFactory("BlockNumber");
-        contract = await factory.deploy();
-        await contract.deployTransaction.wait();
+  beforeEach(async function() {
+    const factory = await ethers.getContractFactory("BlockNumber");
+    contract = await factory.deploy();
+    await contract.deployTransaction.wait();
 
-        accounts = await ethers.getSigners();
-        provider = await ethers.provider;
+    accounts = await ethers.getSigners();
+    provider = await ethers.provider;
+  });
+
+  describe("gambleOnTenthBlockNumber", async function() {
+    it("should reject 0.5 ether", async function() {
+      await provider.send("hardhat_mine", [utils.hexValue(1000)]);
+
+      await expect(
+        contract.gambleOnTenthBlockNumber({
+          value: utils.parseEther("0.5"),
+        })
+      ).to.be.revertedWith("not degen enough");
     });
 
-    describe("gambleOnTenthBlockNumber", async function () {
-        it("should reject 0.5 ether", async function () {
-            await provider.send("hardhat_mine", [utils.hexValue(1000)]);
+    // it("should rejectj if the contract has less than 1 ether", async function () {
+    //     await provider.send("hardhat_mine", [utils.hexValue(1000)]);
 
-            await expect(
-                contract.gambleOnTenthBlockNumber({
-                    value: utils.parseEther("0.5"),
-                }),
-            ).to.be.revertedWith("not degen enough");
-        });
+    //     await expect(
+    //         contract.gambleOnTenthBlockNumber({
+    //             value: utils.parseEther("1"),
+    //         }),
+    //     ).to.be.revertedWith("can't gamble with you");
+    // });
 
-        // it("should rejectj if the contract has less than 1 ether", async function () {
-        //     await provider.send("hardhat_mine", [utils.hexValue(1000)]);
+    it("should revert if the user bets before cooldown time", async function() {
+      await provider.send("hardhat_mine", [utils.hexValue(1000)]);
 
-        //     await expect(
-        //         contract.gambleOnTenthBlockNumber({
-        //             value: utils.parseEther("1"),
-        //         }),
-        //     ).to.be.revertedWith("can't gamble with you");
-        // });
+      await contract.gambleOnTenthBlockNumber({
+        value: utils.parseEther("1"),
+      });
 
-        it("should revert if the user bets before cooldown time", async function () {
-            await provider.send("hardhat_mine", [utils.hexValue(1000)]);
+      await provider.send("hardhat_mine", [utils.hexValue(1)]);
 
-            await contract.gambleOnTenthBlockNumber({
-                value: utils.parseEther("1"),
-            });
-
-            await provider.send("hardhat_mine", [utils.hexValue(1)]);
-
-            await expect(
-                contract.gambleOnTenthBlockNumber({
-                    value: utils.parseEther("1"),
-                }),
-            ).to.be.revertedWith("wait for cooldown time");
-        });
-
-        it("should accept 1 ether", async function () {
-            await provider.send("hardhat_mine", [utils.hexValue(1000)]);
-
-            await contract.gambleOnTenthBlockNumber({
-                value: utils.parseEther("1"),
-            });
-        });
+      await expect(
+        contract.gambleOnTenthBlockNumber({
+          value: utils.parseEther("1"),
+        })
+      ).to.be.revertedWith("wait for cooldown time");
     });
+
+    it("should accept 1 ether", async function() {
+      await provider.send("hardhat_mine", [utils.hexValue(1000)]);
+
+      await contract.gambleOnTenthBlockNumber({
+        value: utils.parseEther("1"),
+      });
+    });
+  });
+
+  describe("claimWinnings", async function() {
+    it("should revet if block.number too small", async function() {
+      const tx = await contract
+        .connect(accounts[0])
+        .gambleOnTenthBlockNumber({ value: utils.parseEther("1") });
+      tx.wait();
+
+      await provider.send("hardhat_mine", [utils.hexValue(19)]);
+
+      await expect(contract.claimWinnings()).to.be.revertedWith("too early");
+    });
+
+    it("should revet if block.number too large", async function() {
+      const tx = await contract
+        .connect(accounts[0])
+        .gambleOnTenthBlockNumber({ value: utils.parseEther("1") });
+      tx.wait();
+
+      await provider.send("hardhat_mine", [utils.hexValue(150)]);
+
+      await expect(contract.claimWinnings()).to.be.revertedWith("too late");
+    });
+
+    it("should accept claimWinnings", async function() {
+      //   const originalBalance = await provider.getBalance(accounts[0].address);
+
+      const tx = await contract
+        .connect(accounts[0])
+        .gambleOnTenthBlockNumber({ value: utils.parseEther("1") });
+      tx.wait();
+
+      await provider.send("hardhat_mine", [utils.hexValue(100)]);
+
+      await expect(contract.connect(accounts[0]).claimWinnings()).to.not
+        .reverted;
+
+      //   expect(await provider.getBalance(accounts[0].address)).to.be.closeTo(
+      //     originalBalance,
+      //     utils.parseEther("0.02")
+      //   );
+    });
+  });
 });
