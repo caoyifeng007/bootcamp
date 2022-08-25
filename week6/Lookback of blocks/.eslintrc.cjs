b164a19edae22fa8cfe0003f9f2aa15ad6e58a1a
ElvinsJ <caoyifeng007@163.com>
2022-08-26 06:44:32 +0800

    Add VeeValidate and Yup use case for monitor component

--- week6/Lookback of blocks/.eslintrc.cjs
+++ week6/Lookback of blocks/.eslintrc.cjs
@@ -12,4 +12,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };
--- week6/Lookback of blocks/src/App.vue
+++ week6/Lookback of blocks/src/App.vue
@@ -2,14 +2,27 @@
   <div class="flex flex-col items-center p-10">
     <div class="flex w-1/3 justify-center space-x-8">
       <div class="flex flex-col w-full">
-        <el-input class="h-10 w-full" v-model="inputAddr" clearable />
+        <el-input
+          class="h-10 w-full"
+          v-model="inputAddr"
+          placeholder="Input the ERC20 token you want to subscribe"
+          name="address"
+          @change="check"
+          clearable
+        />
+        <span>{{ errDisplay }}</span>
 
         <span class="h-10 w-full flex items-center">
           {{ contractAddr }}
         </span>
       </div>
 
-      <el-button class="h-20" @click="renewSubscription" color="#626aef">
+      <el-button
+        class="h-20"
+        @click="renewSubscription"
+        :disabled="subscribeDisable"
+        color="#626aef"
+      >
         Subscribe</el-button
       >
     </div>
@@ -27,6 +40,11 @@ import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useMonitorStore } from "@/stores/monitor";
 
+import { useForm } from "vee-validate";
+// https://vee-validate.logaretm.com/v4/guide/best-practices#yup-bundle-size
+// Yup bundle size
+import { object, string } from "yup";
+
 import LogMonitor from "@/components/LogMonitor.vue";
 import BaseFeeMonitor from "@/components/BaseFeeMonitor.vue";
 import RationMonitor from "@/components/RationMonitor.vue";
@@ -35,11 +53,39 @@ const monitorStore = useMonitorStore();
 const { contractAddr } = storeToRefs(monitorStore);
 
 const inputAddr = ref("");
+const errDisplay = ref("");
+const isValid = ref(false);
+const subscribeDisable = ref(false);
 
 monitorStore.init();
 
+interface Subscribe {
+  address: string;
+}
+const schema = object({
+  address: string().required().min(3),
+});
+// Create a form context with the validation schema
+const { setValues, validate } = useForm<Subscribe>({
+  validationSchema: schema,
+});
+
+async function check() {
+  console.log("change event");
+  setValues({
+    address: inputAddr.value,
+  });
+  const res = await validate();
+  errDisplay.value = res.errors.address!;
+  isValid.value = res.valid;
+  console.log(res.valid);
+}
+
 function renewSubscription() {
-  contractAddr.value = inputAddr.value;
-  monitorStore.init();
+  if (!isValid.value) {
+    return;
+  }
+  //   contractAddr.value = inputAddr.value;
+  console.log("renew");
 }
 </script>
