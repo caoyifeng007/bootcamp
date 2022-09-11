665b0c2b99a4e706a3a19e98c53b0ffce0bdd12e
ElvinsJ <caoyifeng007@163.com>
2022-09-11 10:48:46 +0800

    CaptureTheEther Choose a nickname Complete

--- week5/hardhat-ts-sample/test/Greeter.ts
+++ week5/hardhat-ts-sample/test/Greeter.ts
@@ -3,17 +3,17 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { Greeter } from "../typechain-types";
-
-describe("Greeter", function () {
+const { utils } = ethers;
+describe("Greeter", function() {
     let contract: Greeter;
-    beforeEach(async function () {
+    beforeEach(async function() {
         const ContractFactory = await ethers.getContractFactory("Greeter");
         contract = await ContractFactory.deploy("Hello, world!");
         console.log(contract);
         await contract.deployTransaction.wait();
     });
 
-    it("Should return the new greeting once it's changed", async function () {
+    it("Should return the new greeting once it's changed", async function() {
         expect(await contract.greet()).to.equal("Hello, world!");
 
         const setGreetingTx = await contract.setGreeting("你好");
@@ -21,6 +21,8 @@ describe("Greeter", function () {
 
         // wait until the transaction is mined
         await setGreetingTx.wait();
+
+        console.log(utils.formatBytes32String("ElvinsJ"));
 
         expect(await contract.greet()).to.equal("你好");
     });
