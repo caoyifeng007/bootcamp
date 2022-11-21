341ca7da0790464e4525313dfa8147e115411e46
ElvinsJ <caoyifeng007@163.com>
2022-11-21 17:01:56 +0800

    Adding hardhat-yul plugin

---
+++ week5/hardhat-ts-sample/contracts/Yul ERC1155/IYulERC1155.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/interfaces/IERC1155.sol";
+
+interface MyYulIERC1155 is IERC1155 {}
--- week5/hardhat-ts-sample/hardhat.config.ts
+++ week5/hardhat-ts-sample/hardhat.config.ts
@@ -6,6 +6,8 @@ import "@nomiclabs/hardhat-solhint";
 
 import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
+import "@tovarishfin/hardhat-yul";
+
 // set proxy
 // 为了解决 hh verify 时的bug
 // import { ProxyAgent, setGlobalDispatcher } from "undici";
@@ -13,63 +15,60 @@ import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-d
 // setGlobalDispatcher(proxyAgent);
 
 const config: HardhatUserConfig = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.9",
-            },
-            {
-                version: "0.6.12",
-                settings: {},
-            },
-        ],
-    },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.6.12",
+        settings: {},
+      },
+    ],
+  },
 
-    // defaultNetwork: "rinkeby",
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {
-            forking: {
-                url: `${process.env.ALCHEMY_KEY}`,
-                // url: `${process.env.INFURA_KEY}`,
-                blockNumber: 15352600,
-            },
-            // https://hardhat.org/hardhat-network/docs/overview#logging
-            // This logging is enabled by default when using Hardhat Network's node (i.e. npx hardhat node)
-            // But disabled when using the in-process Hardhat Network provider.
-            // 所以当tx失败，需要查看一下交易详情的时候可以设置为true
-            // loggingEnabled: true,
-        },
-        rinkeby: {
-            url: `${process.env.INFURA_KEY}`,
-            accounts: [
-                `${process.env.ACCOUNT_4_PK}`,
-                `${process.env.ACCOUNT_3_PK}`,
-            ],
-        },
+  // defaultNetwork: "rinkeby",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `${process.env.ALCHEMY_KEY}`,
+        // url: `${process.env.INFURA_KEY}`,
+        blockNumber: 15352600,
+      },
+      // https://hardhat.org/hardhat-network/docs/overview#logging
+      // This logging is enabled by default when using Hardhat Network's node (i.e. npx hardhat node)
+      // But disabled when using the in-process Hardhat Network provider.
+      // 所以当tx失败，需要查看一下交易详情的时候可以设置为true
+      // loggingEnabled: true,
     },
+    rinkeby: {
+      url: `${process.env.INFURA_KEY}`,
+      accounts: [`${process.env.ACCOUNT_4_PK}`, `${process.env.ACCOUNT_3_PK}`],
+    },
+  },
 
-    etherscan: {
-        apiKey: `${process.env.ETHERSCAN_API_KEY}`,
-    },
+  etherscan: {
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
+  },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners();
 
-    for (const account of accounts) {
-        console.log(account.address);
-    }
+  for (const account of accounts) {
+    console.log(account.address);
+  }
 });
 
 // https://hardhat.org/hardhat-runner/plugins/nomiclabs-hardhat-ethers#usage
 task(
-    "blockNumber",
-    "Prints the current block number",
-    async (_, { ethers }) => {
-        await ethers.provider.getBlockNumber().then((blockNumber) => {
-            console.log("Current block number: " + blockNumber);
-        });
-    },
+  "blockNumber",
+  "Prints the current block number",
+  async (_, { ethers }) => {
+    await ethers.provider.getBlockNumber().then((blockNumber) => {
+      console.log("Current block number: " + blockNumber);
+    });
+  }
 );
 export default config;
